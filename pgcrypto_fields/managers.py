062f607d049c303c64e8d31964657c830777d491
Andriy Sokolovskiy <me@asokolovskiy.com>
2015-07-08 10:37:58 +0300

    Used field name with table to prevent ambiguous SQL situations

--- pgcrypto_fields/managers.py
+++ pgcrypto_fields/managers.py
@@ -1,7 +1,6 @@
 from django.conf import settings
 from django.db import models
 
-
 from pgcrypto_fields.mixins import PGPMixin
 
 
@@ -13,13 +12,10 @@ class PGPEncryptedManager(models.Manager):
     def get_queryset(self, *args, **kwargs):
         """Django queryset.extra() is used here to add decryption sql to query."""
         select_sql = {}
-        encrypted_fields_names = []
         for f in self.model._meta.get_fields_with_model():
-            if isinstance(f[0], PGPMixin):
-                encrypted_fields_names.append(f[0].name)
-        for field in encrypted_fields_names:
-            select_sql[field] = "pgp_pub_decrypt({0}, dearmor('{1}'))".format(
-                field, settings.PRIVATE_PGP_KEY,
-            )
-        qs = super(PGPEncryptedManager, self).get_queryset(*args, **kwargs)
-        return qs.extra(select=select_sql)
+            field = f[0]
+            if isinstance(field, PGPMixin):
+                select_sql[field.name] = """pgp_pub_decrypt("{0}"."{1}", dearmor('{2}'))""".format(
+                    field.model._meta.db_table, field.name, settings.PRIVATE_PGP_KEY,
+                )
+        return super(PGPEncryptedManager, self).get_queryset(*args, **kwargs).extra(select=select_sql)
